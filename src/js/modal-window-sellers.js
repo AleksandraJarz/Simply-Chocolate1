699e4615c59ff2f50b1eeffab1ce957597b046e6
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2024-10-14 13:30:36 +0200

    add reviews

---
+++ src/js/modal-window-sellers.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-3]'),
+    closeModalBtn: document.querySelector('[data-modal-close-3]'),
+    modal: document.querySelector('[data-modal-3]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    if (refs.modal.classList.contains('is-hidden')) {
+      refs.body.style.overflow = 'visible';
+    } else {
+      refs.body.style.overflow = 'hidden';
+    }
+  }
+})();
